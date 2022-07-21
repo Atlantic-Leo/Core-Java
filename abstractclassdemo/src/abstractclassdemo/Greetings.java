63d82b245ed4419683d6a41bc746b2318b34b56f
Atlantic-Leo <muralidharpatnaik187@gmail.com>
2022-07-21 15:19:40 +0530

    Core java

---
+++ abstractclassdemo/src/abstractclassdemo/Greetings.java
@@ -0,0 +1,14 @@
+package abstractclassdemo;
+
+public abstract  class Greetings {
+	
+	
+	public abstract void helloName();
+	
+	public void great()
+	{
+		System.out.println("Great!!");
+	}
+
+	
+}
---
+++ abstractclassdemo/src/abstractclassdemo/JackGreets.java
@@ -0,0 +1,23 @@
+package abstractclassdemo;
+
+public class JackGreets extends Greetings {
+	
+	public static void main(String[] args) {
+		JackGreets j = new JackGreets();
+		j.helloName();
+		j.great();
+
+	
+		
+	}
+
+
+	public void helloName() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Jamie");
+		
+	}
+	
+
+}
